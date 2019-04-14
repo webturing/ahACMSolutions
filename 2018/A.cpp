ea9d4dfc4d896140ddf9c860bb37249cee9bb9f5
webturing <zj@webturing.com>
2019-04-14 10:38:57 +0800

    2018 问题 A: 编译错误：  字符串处理：拓扑排序

--- 2018/A.cpp
+++ 2018/A.cpp
@@ -1,31 +1,56 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    char a[11];
-    char b[100010];
-    while (~scanf("%s", a)) {
-        getchar();
-        gets(b);
-        int ans = 0;
-        int lena = strlen(a);
-        int lenb = strlen(b);
-        for (int i = 0; i < lena; i++) {
-            if (a[i] >= 'A' && a[i] <= 'Z')
-                a[i] = a[i] - 'A' + 'a';
-        }
-        for (int i = 0; i < lenb; i++) {
-            if (b[i] >= 'A' && b[i] <= 'Z')
-                b[i] = b[i] - 'A' + 'a';
-        }
-        char *k;
-        k = strstr(b, a);
-        while (k != NULL) {
-            ans++;
-            k = strstr(k + lena, a);
-        }
-        printf("%d\n", ans);
+const int MAXN = 2e5 + 10;
+map<string, int> name;
+vector<int> G[MAXN];
+int in[MAXN];
+int n, pos;
+
+bool tupo() {
+    int cnt = 0;
+    queue<int> que;
+    for (int i = 1; i <= pos; i++) {
+        if (in[i] == 0)
+            que.push(i);
     }
+    while (!que.empty()) {
+        int now = que.front();
+        que.pop();
+        cnt++;
+        for (int i = 0; i < G[now].size(); i++) {
+            if (--in[G[now][i]] == 0)
+                que.push(G[now][i]);
+        }
+    }
+    return cnt == n;
+}
+
+int main() {
+    int t;
+    string a, b;
+    cin >> t;
+    while (t--) {
+        pos = 0;
+        cin >> n;
+        for (int i = 1; i <= 2 * n; i++)
+            G[i].clear();
+        name.clear();
+        memset(in, 0, sizeof(in));
+        for (int i = 1; i <= n; i++) {
+            cin >> a >> b;
+            if (name[a] == 0)
+                name[a] = ++pos;
+            if (name[b] == 0)
+                name[b] = ++pos;
+            G[name[a]].push_back(name[b]);
+            in[name[b]]++;
+        }
+        if (tupo())
+            cout << "Passed" << endl;
+        else
+            cout << "Failed" << endl;
+    }
+
     return 0;
 }
