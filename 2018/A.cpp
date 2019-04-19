0b2587c6205fa9a4168da80d2d2f715eb2bb829b
zj <zj@webturing.com>
2019-04-19 13:19:11 +0800

    数7 模拟题

--- 2018/A.cpp
+++ 2018/A.cpp
@@ -1,56 +1,36 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-const int MAXN = 2e5 + 10;
-map<string, int> name;
-vector<int> G[MAXN];
-int in[MAXN];
-int n, pos;
+inline int f(int n) {
+    int tot = 0;
+    while (n % 7 == 0) {
+        ++tot;
+        n /= 7;
+    }
+    return tot;
+}
 
-bool tupo() {
-    int cnt = 0;
-    queue<int> que;
-    for (int i = 1; i <= pos; i++) {
-        if (in[i] == 0)
-            que.push(i);
+inline int g(int n) {
+    int tot = 0;
+    while (n > 0) {
+        if (n % 10 == 7)++tot;
+        n /= 10;
     }
-    while (!que.empty()) {
-        int now = que.front();
-        que.pop();
-        cnt++;
-        for (int i = 0; i < G[now].size(); i++) {
-            if (--in[G[now][i]] == 0)
-                que.push(G[now][i]);
-        }
-    }
-    return cnt == n;
+    return tot;
 }
 
 int main() {
-    int t;
-    string a, b;
-    cin >> t;
-    while (t--) {
-        pos = 0;
-        cin >> n;
-        for (int i = 1; i <= 2 * n; i++)
-            G[i].clear();
-        name.clear();
-        memset(in, 0, sizeof(in));
-        for (int i = 1; i <= n; i++) {
-            cin >> a >> b;
-            if (name[a] == 0)
-                name[a] = ++pos;
-            if (name[b] == 0)
-                name[b] = ++pos;
-            G[name[a]].push_back(name[b]);
-            in[name[b]]++;
+    int T;
+    cin >> T;
+    while (T--) {
+        int L, R;
+        cin >> L >> R;
+        int tot = 0;
+        for (int n = L; n <= R; n++) {
+            tot += f(n) + g(n);
         }
-        if (tupo())
-            cout << "Passed" << endl;
-        else
-            cout << "Failed" << endl;
+        cout << tot << endl;
     }
-
     return 0;
 }
