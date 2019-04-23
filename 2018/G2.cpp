53ad465f92556c03f25da08faba44222c288e581
webturing <zj@webturing.com>
2019-04-23 16:56:31 +0800

    2018G题炉石传说，然后打五： 动态规划，概率 保证正确，但是可能还要优化一下

--- 2018/G2.cpp
+++ 2018/G2.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 const int maxn = 5000;
-double f[2][maxn + 10];
+double f[maxn + 1][3 * maxn + 10];
 
 int main() {
     int T;
@@ -10,24 +10,24 @@ int main() {
     while (T-- > 0) {
         int n, h;
         cin >> n >> h;
-        if (h <= 3 * n) cout << "0.000000" << endl;
-        else if (h > 6 * n) cout << "1.000000" << endl;
+        if (h < 3 * n) cout << "1.000000" << endl;
+        else if (h > 6 * n) cout << "0.000000" << endl;
         else {
             h -= 3 * n;
             memset(f, 0, sizeof(f));
-            f[1][0] = 1;
-            f[1][1] = 0.75;
-            f[1][2] = 0.5;
+            f[1][0] = 0.25;
+            f[1][1] = 0.25;
+            f[1][2] = 0.25;
             f[1][3] = 0.25;
             for (int i = 2; i <= n; i++) {
                 for (int j = 0; j <= 3 * i; j++) {
                     f[i][j] = 0;
-                    for (int k = 0; k <= j && k <= 3; k++) {
-                        f[i][j] += f[(i + 1) % 2][j - k] * 0.25;
+                    for (int k = 0; k <= min(3, j); k++) {
+                        f[i][j] += 0.25 * f[i - 1][j - k];
                     }
                 }
             }
-            cout << fixed << setprecision(6) << f[n][h] << endl;
+            cout << fixed << setprecision(6) << accumulate(f[n] + h, f[n] + 3 * n + 1, 0.0) << '\n';
         }
     }
     return 0;
