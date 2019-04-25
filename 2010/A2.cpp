51dc0ad492204c75f7b75cc744a4937bc0a6d9bd
webturing <zj@webturing.com>
2019-04-26 00:07:47 +0800

    STL 字符串 find transform _tolower

---
+++ 2010/A2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string a, b;
+
+    while (getline(cin, a)) {
+        getline(cin, b);
+        transform(a.begin(), a.end(), a.begin(), _tolower);
+        transform(b.begin(), b.end(), b.begin(), _tolower);
+
+        int ans = 0;
+        for (auto p = b.find(a); p != string::npos; p = b.find(a, p + a.size())) {
+            ++ans;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
