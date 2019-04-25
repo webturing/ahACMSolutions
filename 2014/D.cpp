c9377dd1d1a72f4a89baf2434e67d0d133cc5975
webturing <zj@webturing.com>
2019-04-25 20:59:15 +0800

    D路边骗局 博弈

---
+++ 2014/D.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int n;
+    // freopen("D.in", "r", stdin);
+    // freopen("D.out", "w", stdout);
+    while (scanf("%d", &n) != EOF) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int x;
+            scanf("%d", &x);
+            sum ^= x;
+        }
+        if (sum)
+            printf("Yes\n");
+        else
+            printf("No\n");
+    }
+    return 0;
+}
