4dda6fb49bb482733f2e91dca10e5179389240b5
webturing <zj@webturing.com>
2019-04-26 11:08:28 +0800

    STL string transform _lower string::find getline

--- 2010/A3.cpp
+++ 2010/A3.cpp
@@ -6,15 +6,11 @@ using namespace std;
 int main() {
     string a, b;
 
-    while (getline(cin, a)) {
-        getline(cin, b);
+    while (getline(cin, a) && etline(cin, b)) {
         transform(a.begin(), a.end(), a.begin(), _tolower);
         transform(b.begin(), b.end(), b.begin(), _tolower);
-
         int ans = 0;
-        for (auto p = b.find(a); p != string::npos; p = b.find(a, p + a.size())) {
-            ++ans;
-        }
+        for (auto p = b.find(a); p != string::npos; p = b.find(a, p + a.size())) ++ans;
         cout << ans << endl;
     }
     return 0;
